883e29fa8b27037c5336707ce86353e9f80a5ed8
abdu951 <abdumoh051@gmail.com>
2026-02-25 23:14:28 +0300

    c

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,10 +5,17 @@ const Hero = () => {
     const [mobileOpen, setMobileOpen] = React.useState(false);
 
     return (
-        <div className="flex flex-col items-center justify-center pb-48 text-center text-sm text-white max-md:px-2 bg-cover bg-center h-screen relative" style={{ backgroundImage: `linear-gradient(rgba(1, 0, 3, 0.65), rgba(1, 0, 7, 0.65)), url(${bgImage})` }}>
-            <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">Explore The World with <span>Future quest</span></h1>
-            <p>orem ipsum dolor amet, consetetur sadipscing elitr sed diam nonumy eirmod tempor invidun dolore.</p>
-            <button type="button" className="w-40 py-3 active:scale-95 transition text-sm text-white rounded-full bg-slate-700"><p className="mb-0.5">book now</p></button>
+        <div className="pt-10 flex items-center justify-between px-4 pb-48 text-center text-sm text-white max-md:px-2 bg-cover bg-center h-screen relative" style={{ backgroundImage: `linear-gradient(rgba(1, 0, 3, 0.65), rgba(1, 0, 7, 0.65)), url(${bgImage})` }}>
+            <div>
+                <h1 className="pb-10 mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">Explore The World with <span className='text-[#ffa843]'>Future quest</span></h1>
+                <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">orem ipsum dolor amet, consetetur sadipscing elitr sed diam nonumy eirmod tempor invidun dolore.</p>
+                <button type="button" className="w-40 py-3 active:scale-95 transition text-sm text-white rounded-full bg-[#ffa843]">book now</button>
+            </div>
+
+            <div>
+
+            </div>
+            
         </div>
     )
 }
